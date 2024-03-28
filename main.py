ae4328c1654e1740bc554f4c9d5d52bad24fc01f
Kingdawnage <katchymemo@yahoo.com>
2024-03-28 21:35:50 +0100

    Updated the PR structure to conform to the latest PR commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from google.oauth2 import credentials
 from googleapiclient.discovery import build
 from certificateEditor import edit_certificate, font_path
 import csvReader
+import driveUpload
 
 
 attendees_list = csvReader.read_names_from_file(csvReader.filename)
@@ -9,3 +10,6 @@ attendees_list = csvReader.read_names_from_file(csvReader.filename)
 print(attendees_list)
 
 edit_certificate("Certificates_GDSC_BME_page-0001.jpg", attendees_list, font_path)
+
+# Use case example: Upload("path/to/folder or file", "Name of drive folder to upload to")
+driveUpload.Upload("Certificates_GDSC_BME", "Certificates")
